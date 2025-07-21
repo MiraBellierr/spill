6c0540ec15d76dbb651c94576353c27b53819032
Mirabellier <75848@siswa.unimas.my>
2025-07-21 18:28:57 +0800

    feat: add react-helmet for improved SEO and page metadata management

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import { Helmet } from "react-helmet";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Blog from "./pages/Blog";
@@ -8,6 +9,21 @@ import BlogEdit from "./pages/BlogEdit";
 function App() {
   return (
     <div>
+      <Helmet>
+        <title>Mirabellier ⭐</title>
+        <link rel="icon" href="https://mirabellier.my.id/favicon.jpg" />
+        <meta name="theme-color" content="#EE82EE" />
+        <meta content="Mirabellier ⭐ | My blog" property="og:title" />
+        <meta content="This site is just my little corner of the web where I share my thoughts, memories, and maybe some projects I’m working on! I might add more pages soon." property="og:description" />
+        <meta content="Mirabellier" property="og:site_name" />
+        <meta content='https://mirabellier.my.id/favicon.jpg' property='og:image' />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="Mirabellier ⭐" />
+        <meta name="twitter:creator" content="@mirabellier" />
+        <meta name="twitter:title" content="Mirabellier ⭐ | My blog" />
+        <meta name="twitter:description" content="This site is just my little corner of the web where I share my thoughts, memories, and maybe some projects I’m working on! I might add more pages soon." />
+        <meta name="twitter:image" content="https://mirabellier.my.id/background.jpg" />
+      </Helmet>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/spill" element={<Home />} />
