2d5940d52a63821c3eeb40c85e20e956003cf59b
Mirabellier <75848@siswa.unimas.my>
2025-07-26 01:25:03 +0800

    span to h1

--- src/parts/Header.tsx
+++ src/parts/Header.tsx
@@ -1,7 +1,7 @@
 const Header = () => {
     return (
      <header className="bg-blue-200 border-b-2 border-blue-300 p-4 text-center text-4xl font-bold text-blue-700 shadow-sm">
-        <span className="tracking-widest">Welcome to my website</span>
+        <h1 className="tracking-widest">Welcome to my website</h1>
       </header>
     )
 }
